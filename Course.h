8c40d3b751bdb422a7c3037a531a77f1af107906
BigM3za <mazenads971@gmail.com>
2025-11-11 09:02:47 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,25 @@
 #ifndef COURSE_H
 #define COURSE_H
 
-#include <string>
 #include "Student.h"
-using namespace std;
-//#write your code here
+#include "Instructor.h"
 
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    Instructor* instructor;
 
-
-
-
-
-
-
-
-
-
-
-
+public:
+    Course();
+    Course(string code, string name, int max);
+    ~Course();
+    void setInstructor(Instructor* inst);
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+};
 
 #endif
