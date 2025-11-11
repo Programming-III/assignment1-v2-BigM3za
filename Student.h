dd3ca006c41c412cb429241d3fd7c6148e55be33
BigM3za <mazenads971@gmail.com>
2025-11-11 09:01:52 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -2,20 +2,16 @@
 #define STUDENT_H
 
 #include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
 
-
-
-
-
-
-
-
-
-
-
+public:
+    Student();
+    Student(string name, int id, int yearLevel, string major);
+    void display();
+};
 
 #endif
