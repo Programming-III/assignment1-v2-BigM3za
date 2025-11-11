4997aff1ffcbeedfb0f28b7be5a04551cf2c0fa6
BigM3za <mazenads971@gmail.com>
2025-11-11 09:01:13 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,22 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <iostream>
 #include <string>
+
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
 
-
-
-
-
-
-
-
-
-
-
+public:
+    Person();
+    Person(string name, int id);
+    string getName();
+    int getID();
+    void display();
+};
 
 #endif
